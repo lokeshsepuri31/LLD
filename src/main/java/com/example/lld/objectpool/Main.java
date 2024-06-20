84a122eac6038d628495eda1a6e0a6d565f5593a
lokesh <lokeshkumartcs2020@gmail.com>
2024-06-20 11:44:21 +0530

    Added main file in objectpool

--- src/main/java/com/example/lld/objectpool/Main.java
+++ src/main/java/com/example/lld/objectpool/Main.java
@@ -2,4 +2,11 @@ package com.example.lld.objectpool;
 
 public class Main {
 
+	public static void main(String[] args) {
+		DBConnectionPoolManager connectionPoolManager = DBConnectionPoolManager.getInstance();
+		DBConnection connection = connectionPoolManager.getConnection();
+
+		connectionPoolManager.releaseConnection(connection);
+
+	}
 }
